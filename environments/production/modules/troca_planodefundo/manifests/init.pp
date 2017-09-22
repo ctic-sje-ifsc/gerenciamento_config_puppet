c80d68c3f11e39c30911d89ccc9bf8937cf10a70
souzagaabriel <souzagaabriel@gmail.com>
2017-09-22 16:37:48 -0300

    Módulo troca_planofundo

---
+++ environments/production/modules/troca_planodefundo/manifests/init.pp
@@ -0,0 +1,24 @@
+class troca_planodefundo
+ {
+
+if $::kernel == windows {
+    file {'win_fundo_ifsc_sj.jpg':
+      path => 'C:\Users\fundo_ifsc_sj.jpg',
+      ensure => file,
+      source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
+      source_permissions => ignore,
+    }
+}else{
+	if $lsbdistcodename == trusty {
+        file { 'lin_fundo_ifsc_sj.jpg':
+                path => '/home/imagens/fundo_ifsc_sj.jpg',
+                ensure => file,
+                source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
+                owner => root,
+                group => root,
+                mode => 0755,
+        }
+	}
+
+  }
+}
