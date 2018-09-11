039d3d97cc477b657445cffebb816e0ce16e114c
Gabriel de Souza <souzagaabriel@gmail.com>
2018-09-10 21:29:59 -0300

    Adicionar opção de mudar plano de fundo do Debian8.

--- environments/production/modules/troca_planodefundo/manifests/init.pp
+++ environments/production/modules/troca_planodefundo/manifests/init.pp
@@ -1,24 +1,44 @@
-class troca_planodefundo
- {
+class troca_planodefundo {
 
-if $::kernel == windows {
-    file {'win_fundo_ifsc_sj.jpg':
-      path => 'C:\Users\fundo_ifsc_sj.jpg',
-      ensure => file,
-      source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
-      source_permissions => ignore,
+  if $::kernel == windows {
+      file {'win_fundo_ifsc_sj.jpg':
+        path => 'C:\Users\fundo_ifsc_sj.jpg',
+        ensure => file,
+        source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
+        source_permissions => ignore,
+      }
+  }else{
+    if $lsbdistcodename == trusty {
+          file { 'lin_fundo_ifsc_sj.jpg':
+                  path => '/home/imagens/fundo_ifsc_sj.jpg',
+                  ensure => file,
+                  source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
+                  owner => root,
+                  group => root,
+                  mode => 0755,
+          }
     }
-}else{
-	if $lsbdistcodename == trusty {
-        file { 'lin_fundo_ifsc_sj.jpg':
-                path => '/home/imagens/fundo_ifsc_sj.jpg',
-                ensure => file,
-                source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
-                owner => root,
-                group => root,
-                mode => 0755,
-        }
-	}
 
+    if $lsbdistcodename == jessie {
+          file { 'lin_fundo_ifsc_sj.jpg':
+                  path => '/home/fundo_ifsc_sj.jpg',
+                  ensure => file,
+                  source => 'puppet:///modules/troca_planodefundo/fundo_ifsc_sj.jpg',
+                  owner => root,
+                  group => root,
+                  mode => 0755,
+          }
+
+          file { 'trocaplanodefundo.sh':
+                  path => '/etc/profile.d/trocaplanodefundo.sh',
+                  ensure => file,
+                  source => 'puppet:///modules/troca_planodefundo/trocaplanodefundo.sh',
+                  owner => root,
+                  group => root,
+                  mode => 0755,
+                  require => file['lin_fundo_ifsc_sj.jpg'],
+          }
+        
+    }
   }
 }
