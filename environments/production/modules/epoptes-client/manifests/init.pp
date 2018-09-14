7ed24b9e33c9634383021bcc32f2689df4cb0cb5
Gabriel de Souza <souzagaabriel@gmail.com>
2018-09-14 13:42:10 -0300

    Corrigir IPs server epoptes labs CAD

--- environments/production/modules/epoptes-client/manifests/init.pp
+++ environments/production/modules/epoptes-client/manifests/init.pp
@@ -10,9 +10,6 @@ class epoptes-client {
 	case $hostname {
 		/^sj-labalu-/: {$epoptes_server = '192.168.4.254'}
 		/sj-labin/: {$epoptes_server = '172.18.135.1'}
-		/^sj-labcad1/: {$epoptes_server = '172.18.132.1'}
-		/^sj-labcad2/: {$epoptes_server = '172.18.133.1'}
-		/^sj-labcad3/: {$epoptes_server = '172.18.134.1'}
 		default: { $epoptes_server = '192.168.1.1' }
 	}
 
@@ -26,15 +23,4 @@ class epoptes-client {
   		minute  => '*/5',
 			ensure => absent,
 	 }
-/* ANTIGO USO DO RCLOCAL COM CONCAT
-		adiciona no rc.local o epoptes para se pegar o certificado do server e iniciar
-		necessario usar o modulo rclocal
-
-	concat::fragment { '04_rc.local_epoptes_config':
-        target => '/etc/rc.local',
-        #content => "# Adquire o certificado do server e iniciar o epoptes-client\nepoptes-client -c\nepoptes-client&\n",
-        content => "# Adquire o certificado do server e iniciar o epoptes-client\nepoptes-client -c",
-        order => '04',
-    }
-*/
 }
