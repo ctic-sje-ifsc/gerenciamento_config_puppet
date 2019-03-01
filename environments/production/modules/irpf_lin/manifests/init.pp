b83d3e2dbf46a8857fd8ea0e566709e0173a0441
ricardoifsc <ricardo.ifsc@gmail.com>
2019-03-01 15:45:42 -0300

    atualizacao irpf

--- environments/production/modules/irpf_lin/manifests/init.pp
+++ environments/production/modules/irpf_lin/manifests/init.pp
@@ -1,31 +1,31 @@
 class irpf_lin {
 
-	file { 'IRPF2018-desktop.desktop':
-		path => '/usr/share/applications/IRPF2018-desktop.desktop',
+	file { 'IRPF2019-desktop.desktop':
+		path => '/usr/share/applications/IRPF2019-desktop.desktop',
 		ensure => file,
-		source => 'puppet:///modules/irpf_lin/IRPF2018-desktop.desktop',
+		source => 'puppet:///modules/irpf_lin/IRPF2019-desktop.desktop',
 		owner => root,
 		group => root,
 		mode => 0644,
 	}
 
 	# Comandos para baixar o instalador
-	file { 'irpf18.tgz':
-		path => '/usr/local/src/irpf18.tgz',
+	file { 'irpf19.tgz':
+		path => '/usr/local/src/irpf19.tgz',
 		ensure => file,
-		source => 'puppet:///modules/irpf_lin/irpf18.tgz',
+		source => 'puppet:///modules/irpf_lin/irpf19.tgz',
 		owner => root,
 		group => root,
 		mode => 777,
 	}
 
-	exec { 'descompactar_irpf18.tgz':
-		command => '/bin/tar -x -z -C /usr/local/ -f /usr/local/src/irpf18.tgz ; /bin/chown -R root.root /usr/local/IRPF2018 ; /bin/chmod -R 1777 /usr/local/IRPF2018',
+	exec { 'descompactar_irpf19.tgz':
+		command => '/bin/tar -x -z -C /usr/local/ -f /usr/local/src/irpf19.tgz ; /bin/chown -R root.root /usr/local/IRPF2019 ; /bin/chmod -R 1777 /usr/local/IRPF2019',
 		require => [
-				File['irpf18.tgz'],
+				File['irpf19.tgz'],
 		],
 		# So executa se nao tiver o arquivo abaixo
-		creates => '/usr/local/IRPF2018/irpf.jar',
+		creates => '/usr/local/IRPF2019/irpf.jar',
 		timeout => 0,
  }
 # Removendo versao anterior
